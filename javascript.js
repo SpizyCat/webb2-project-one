d3443ca250deb9ce5f44e764e25d86e476a2c621
SpizyCat <spizy.yt@gmail.com>
2021-10-29 11:04:48 +0200

    Final Commit.

--- javascript.js
+++ javascript.js
@@ -1,19 +1,40 @@
 var currentSlide = 0;
 
-function openModal(){
+function openModal(index){
     modal = document.querySelector(".modal");
     modal.style.display = "flex";
+    currentSlide = index;
     setCurrentImage()
     document.body.className = "bodyclass";
+}
+
+function closeModal(){
+    modal = document.querySelector(".modal");
+    modal.style.display = "none";
+    document.querySelector("body").classList.remove("bodyclass");
 }
 
 function setCurrentImage() {
     modalImage = document.querySelector("#modalImage")
     cars = document.querySelectorAll(".shop");
-    modalImage.src = cars.at(currentSlide).childNodes[1].src;
+    modalImage.src = cars[currentSlide].childNodes[1].src;
 }
 
 function changeSlide(i) {
     currentSlide += i;
     setCurrentImage();
 }
+
+var audio;
+function myAudioFunction(){
+    if (audio){
+        audio.pause()
+        audio.currentTime = 0;
+        audio = null;
+    }
+    else{
+    audio = new Audio('trendy.mp3');
+    audio.play();
+    }
+}
+
